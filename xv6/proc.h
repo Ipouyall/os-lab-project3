462d42fc19f3041e736f0f6b148939f6bbaa85c4
pouya sadeghi <pouyasadeghi2012@gmail.com>
2022-12-11 19:26:02 +0330

    add costume field to proc

--- xv6/proc.h
+++ xv6/proc.h
@@ -49,6 +49,10 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int queue;                   // queue number
+  int creation_time;           // creation time
+  int last_cpu_time;           // last cpu time
+  int age;                     // age
 };
 
 // Process memory is laid out contiguously, low addresses first:
